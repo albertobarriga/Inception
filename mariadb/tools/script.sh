c4256add8664dc9b249d4547cfc6b7ddf0f1c9fe
abarriga <abarriga4242@gmail.com>
2023-11-24 19:53:51 +0100

    problem with the database and users

--- mariadb/tools/script.sh
+++ mariadb/tools/script.sh
@@ -3,8 +3,8 @@
 
 
 # MYSQL SETUP
-db1_name=papitafrita
-db1_user=ratatatta
+db1_name=papafrita
+db1_user=rata
 db1_pwd=tete
 
 
@@ -13,24 +13,16 @@ ftp_user=loscompadres
 ftp_pwd=lanoria
 
 # inicia el servivio de mysql
-service mysql start 
+service mariadb start 
 
+mysql -u root -p '$db1_pwd'
 # crea una base de datos si no existe
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
+mysql -e "CREATE DATABASE IF NOT EXISTS $db1_name ;"
 # crea un usuario si no existe con una contraseña especifica
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
+mysql -e "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;"
 # da todos los privilegios en la base de datos de al usuario
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
+mysql -e "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;"
 # cambia la contraseña a "12345"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
 # actualiza los privilegios
-echo "FLUSH PRIVILEGES;" >> db1.sql
-
-# ejecuta comandos sql 
-mysql < db1.sql
-
-# detenemos el proceso de mysql
-# kill $(cat /var/run/mysqld/mysqld.pid)
-
-# iniciamos el servidor de mysql
-# mysqld
+mysql -e "FLUSH PRIVILEGES;"
