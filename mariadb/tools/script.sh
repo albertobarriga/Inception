2933646267321292ff8e0a417365baf189a6e349
abarriga <abarriga4242@gmail.com>
2023-11-26 18:47:53 +0100

    mariadb working, fix env in compose

--- mariadb/tools/script.sh
+++ mariadb/tools/script.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-
-
-# MYSQL SETUP
-db1_name=papafrita
-db1_user=rata
-db1_pwd=tete
-
-
-# FTP SETUP
-ftp_user=loscompadres
-ftp_pwd=lanoria
-
+# a esperas de tener el docker compose y poder quitarlo de aquí
+DB_USER=abarriga
+DB_USER2=user2
+DB_PASSWORD="12345"
+DB_PASSWORD2="12345"
+DB_NAME=wordpress
+DB_HOST=mariadb
+DB_EMAIL=abarriga@studen.42malaga.com
+DB_EMAIL2=user2@studen.42malaga.com
+URL=abarriga.42.fr
 # inicia el servivio de mysql
-service mariadb start 
+service mysql start 
 
-mysql -u root -p '$db1_pwd'
-# crea una base de datos si no existe
-mysql -e "CREATE DATABASE IF NOT EXISTS $db1_name ;"
-# crea un usuario si no existe con una contraseña especifica
-mysql -e "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;"
-# da todos los privilegios en la base de datos de al usuario
-mysql -e "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;"
-# cambia la contraseña a "12345"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
-# actualiza los privilegios
 mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER@wordpress.srcs_incetion' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER IDENTIFIED BY '$DB_PASSWORD';"
+mysql -e "ALTER USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqld_safe
